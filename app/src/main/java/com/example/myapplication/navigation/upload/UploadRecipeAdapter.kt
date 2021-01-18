36cc3e7ddee7e98c226ab921dff66f26653d5179
onxmoreplz <hamdoyoung95@gmail.com>
2021-01-18 23:24:47 +0900

    프로젝트 모듈화
    - Import Optimization
    - 파일 이름 변경
    - 패키지 생성 및 이름 변경

--- app/src/main/java/com/example/myapplication/navigation/upload/UploadRecipeAdapter.kt
+++ app/src/main/java/com/example/myapplication/navigation/upload/UploadRecipeAdapter.kt
@@ -2,7 +2,7 @@
  *  각각의 item을 recyclerview로 연결 시켜주는 adapter class
  */
 
-package com.example.myapplication
+package com.example.myapplication.navigation.upload
 
 import android.content.Context
 import android.net.Uri
@@ -11,13 +11,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.R
 import kotlinx.android.synthetic.main.recipe_list_item.view.*
 
 /**
  *  기존의 어댑터에서 '갤러리' 버튼 클릭 시 itemClick 이벤트가 생기게 추가하였습니다.
  */
-class RecipeAdapter(private val context: Context, private val recipeList: ArrayList<RecipeData>, val itemClick:(Int, RecipeData) -> Unit) :
-    RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder>() {
+class UploadRecipeAdapter(private val context: Context, private val recipeList: ArrayList<UploadRecipeDTO>, val itemClick:(Int, UploadRecipeDTO) -> Unit) :
+    RecyclerView.Adapter<UploadRecipeAdapter.RecipeViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(
@@ -49,7 +50,7 @@ class RecipeAdapter(private val context: Context, private val recipeList: ArrayL
         private val recipePhoto: ImageView = itemView.iv_photo
         private val recipeButton: Button = itemView.btn_gallery
 
-        fun bind(data: RecipeData) {
+        fun bind(data: UploadRecipeDTO) {
             recipeNumber.text = data.number
 
             if(data.image != "") {
---
+++ app/src/main/java/com/example/myapplication/navigation/upload/UploadRecipeDTO.kt
@@ -0,0 +1,11 @@
+/**
+ *  데이터 초기화 클래스
+ */
+
+package com.example.myapplication.navigation.upload
+
+data class UploadRecipeDTO(
+    val number: String,
+    var comment: String?,
+    var image: String?
+)
