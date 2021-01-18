061608b2eb498eba66ade830a8bba1bf947858cf
parksh5061 <42669772+parksh5061@users.noreply.github.com>
2021-01-18 20:10:44 +0900

    Create RecipeAdapater
    adapter, data calss, viewHolder 생성

---
+++ app/src/main/java/com/example/myapplication/RecipeData.kt
@@ -0,0 +1,10 @@
+/**
+ *  데이터 초기화 클래스
+ */
+
+package com.example.myapplication
+
+import android.net.Uri
+import android.widget.ImageView
+
+data class RecipeData(val number: String, var comment: String?, var image: String?)
