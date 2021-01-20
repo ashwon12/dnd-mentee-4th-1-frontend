ea32c70f17f9bf14b4c28989d2646439ea663e78
onxmoreplz <hamdoyoung95@gmail.com>
2021-01-20 20:37:17 +0900

    [Update] 타임라인(Timeline) 게시물 리사이클려뷰 내 게시물 업데이트 함수 추가

--- app/src/main/java/com/example/myapplication/navigation/timeline/TimelineRecyclerAdapter.kt
+++ app/src/main/java/com/example/myapplication/navigation/timeline/TimelineRecyclerAdapter.kt
@@ -13,10 +13,20 @@ import com.example.myapplication.data.datasource.remote.api.RecipeDTO
 class TimelineRecyclerAdapter(private val items: ArrayList<RecipeDTO.Timeline>) :
     RecyclerView.Adapter<TimelineRecyclerViewHolder>() {
 
+    /**
+     *  TimelineRecyclerAdapter 안에 보여지는 Timeline들 update
+     * */
+    fun updateTimelineList(timelines: List<RecipeDTO.Timeline>) {
+        this.items.clear()
+        this.items.addAll(timelines)
+        notifyDataSetChanged()
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TimelineRecyclerViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(
             R.layout.list_item,
-            parent, false)
+            parent, false
+        )
         return TimelineRecyclerViewHolder(view)
     }
 
