21e6ffc3c0568eb245e6008747b211198d43be90
onxmoreplz <hamdoyoung95@gmail.com>
2021-01-20 20:30:50 +0900

    [Update]  POST방식 요청 메서드 postTimeline 추가

--- app/src/main/java/com/example/myapplication/data/datasource/remote/api/RecipeApi.kt
+++ app/src/main/java/com/example/myapplication/data/datasource/remote/api/RecipeApi.kt
@@ -3,14 +3,21 @@ package com.example.myapplication.data.datasource.remote.api
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.GET
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface RecipeApi {
 
     @GET("v1/search/timeline.json")
     fun getAllTimelines(
         @Query("id") id: String? = null
+    ): Call<RecipeDTO.TimelineResponse>
+
+    @FormUrlEncoded
+    @POST("/posts")
+    fun postTimeline(
+        @Field("title") title: String,
+        @Field("subTitle") subTitle: String,
+        @Field("imageUrls") imageUrls: List<String>
     ): Call<RecipeDTO.TimelineResponse>
 
     companion object {
