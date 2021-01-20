51987a4f20f122ffac61abd15b211a92ba21129a
김재원 <ashwon12@naver.com>
2021-01-21 04:06:41 +0900

    [Feat] 클릭이벤트 추가
    - 아이템 클릭시 모든 데이터 (임시로) 토스트 띄어주기
    - 삭제,수정 버튼시 클릭시 해당 id 보여주기

--- app/src/main/java/com/example/myapplication/navigation/timeline/TimelineFragment.kt
+++ app/src/main/java/com/example/myapplication/navigation/timeline/TimelineFragment.kt
@@ -6,17 +6,23 @@
 
 package com.example.myapplication.navigation.timeline
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.App
+import com.example.myapplication.MainActivity
 import com.example.myapplication.R
 import com.example.myapplication.data.datasource.remote.api.RecipeDTO
 import com.example.myapplication.data.repository.RepositoryImpl
+import java.text.FieldPosition
 
-class TimelineFragment : Fragment() {
+class TimelineFragment : Fragment(),TimelineRecyclerInterface {
 
     private lateinit var v : View
   
@@ -39,6 +45,9 @@ class TimelineFragment : Fragment() {
      */
     fun callRecycler(){
         list.clear()
+        list.add(RecipeDTO.Timeline("0","여기는 제목이에요~~", "여기는 subTitle이에요~~~",
+            listOf("https://image-notepet.akamaized.net/resize/620x-/seimage/20191114%2F6a4c967c5b14197dd5d2c47424ae8e82.jpg")
+        ))
         list.add(RecipeDTO.Timeline("1","여기는 제목이에요~~", "여기는 subTitle이에요~~~",
             listOf("https://image-notepet.akamaized.net/resize/620x-/seimage/20191114%2F6a4c967c5b14197dd5d2c47424ae8e82.jpg")
         ))
@@ -58,11 +67,17 @@ class TimelineFragment : Fragment() {
 
             }
         )
-        myAdapter = TimelineRecyclerAdapter(list)
+        myAdapter = TimelineRecyclerAdapter(this)
+        myAdapter.updateTimelineList(list)
         myAdapter.notifyDataSetChanged()
 
         var rv_list = v.findViewById(R.id.rv_list) as RecyclerView
         rv_list.adapter = myAdapter
     }
 
+    override fun onItemClicked(position: Int) {
+        Log.d("로그", "TimeLinFragment - 클릭됨")
+        Toast.makeText(App.instance,"id : ${this.list[position].id}\n Title : ${this.list[position].title}\n subTitle: ${this.list[position].subTitle}",Toast.LENGTH_SHORT).show()
+
+    }
 }
--- app/src/main/java/com/example/myapplication/navigation/timeline/TimelineRecyclerViewHolder.kt
+++ app/src/main/java/com/example/myapplication/navigation/timeline/TimelineRecyclerViewHolder.kt
@@ -3,8 +3,9 @@
  */
 package com.example.myapplication.navigation.timeline
 
-import android.content.Context
+import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -13,17 +14,41 @@ import com.example.myapplication.App
 import com.example.myapplication.R
 import com.example.myapplication.data.datasource.remote.api.RecipeDTO
 
-class TimelineRecyclerViewHolder(v : View) : RecyclerView.ViewHolder(v) {
-
+class TimelineRecyclerViewHolder(v : View,
+                                 recyclerInterface: TimelineRecyclerInterface) : RecyclerView.ViewHolder(v),
+                                View.OnClickListener
+{
     private val title = v.findViewById<TextView>(R.id.tv_title)
     private val subtitle = v.findViewById<TextView>(R.id.tv_subtitle)
     private val image = v.findViewById<ImageView>(R.id.iv_image)
+    private val btn_delete = v.findViewById<Button>(R.id.btn_delete)
+    private val btn_modify = v.findViewById<Button>(R.id.btn_modify)
 
+    private var myInterface : TimelineRecyclerInterface? = null
+
+    //기본 생성자
+    init {
+        v.setOnClickListener(this)
+        this.myInterface = recyclerInterface
+    }
 
     fun bind(data : RecipeDTO.Timeline){
         title.text = data.title
         subtitle.text = data.subTitle
         Glide.with(App.instance).load(data.imageUrl[0]).into(image);
+
+        //TODO(삭제 버튼 클릭했을 때 해당 데이터 지우는 코드 )
+        btn_delete.setOnClickListener{
+            Log.d("btn_delete","${adapterPosition}")
+        }
+
+        btn_modify.setOnClickListener{
+            Log.d("btn_remove","${adapterPosition}")
+        }
+    }
+
+    override fun onClick(p0: View?) {
+        this.myInterface?.onItemClicked(adapterPosition)
     }
 }
 
