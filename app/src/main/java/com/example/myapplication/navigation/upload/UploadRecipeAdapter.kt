0a1242f1952216ba91c4fe47cbf33e1f244490b8
parksh5061 <42669772+parksh5061@users.noreply.github.com>
2021-01-21 23:10:43 +0900

    [Feat] 갤러리 퍼미션 기능 추가

--- app/src/main/java/com/example/myapplication/navigation/upload/UploadRecipeAdapter.kt
+++ app/src/main/java/com/example/myapplication/navigation/upload/UploadRecipeAdapter.kt
@@ -5,6 +5,7 @@
 package com.example.myapplication.navigation.upload
 
 import android.content.Context
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.text.Editable
 import android.text.TextWatcher
@@ -13,18 +14,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import androidx.core.app.ActivityCompat.requestPermissions
+import androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale
+import androidx.core.content.ContextCompat.checkSelfPermission
+import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.myapplication.R
-import com.example.myapplication.data.datasource.remote.api.PostItem
+import com.example.myapplication.data.datasource.remote.api.RecipeDTO
 import kotlinx.android.synthetic.main.recipe_list_item.view.*
+import java.util.jar.Manifest
 
 /**
  *  기존의 어댑터에서 '갤러리' 버튼 클릭 시 itemClick 이벤트가 생기게 추가하였습니다.
  */
 class UploadRecipeAdapter(
     private val context: Context,
-    private val recipeList: ArrayList<PostItem.Recipe>,
-    val itemClick: (Int, PostItem.Recipe) -> Unit
+    private val recipeList: ArrayList<RecipeDTO.Recipe>,
+    val itemClick: (Int, RecipeDTO.Recipe) -> Unit
 ) :
     RecyclerView.Adapter<UploadRecipeAdapter.RecipeViewHolder>() {
 
@@ -67,18 +74,22 @@ class UploadRecipeAdapter(
         private val recipeNumber: TextView = itemView.tv_number
         var recipeComment: EditText = itemView.et_comment
         private val recipePhoto: ImageView = itemView.iv_photo
-        private val recipeButton: Button = itemView.btn_gallery
+        // private val recipeButton: Button = itemView.btn_gallery
 
-        fun bind(data: PostItem.Recipe) {
+        fun bind(data: RecipeDTO.Recipe) {
             recipeNumber.text = data.number
 
             if (data.image != "") {
-                recipePhoto.setImageURI(Uri.parse(data.image))
+                Glide.with(itemView)
+                    .load(data.image)
+                    .into(recipePhoto)
             } else {
-                recipePhoto.setImageURI(Uri.parse(""))
+                Glide.with(itemView)
+                    .load(R.drawable.gallery)
+                    .into(recipePhoto)
             }
 
-            recipeButton.setOnClickListener {
+            recipePhoto.setOnClickListener {
                 itemClick(adapterPosition, data)
             }
 
