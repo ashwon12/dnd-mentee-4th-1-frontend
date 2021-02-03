c2ab02c8399b6713b775ad36ed50feaa3f095cf8
parksh5061 <42669772+parksh5061@users.noreply.github.com>
2021-02-03 22:00:15 +0900

    [Update] 레시피등록 어댑터 수정

--- app/src/main/java/com/example/myapplication/navigation/upload/UploadRecipeAdapter.kt
+++ app/src/main/java/com/example/myapplication/navigation/upload/UploadRecipeAdapter.kt
@@ -1,68 +1,46 @@
-/**
- *  각각의 item을 recyclerview로 연결 시켜주는 adapter class
- */
-
 package com.example.myapplication.navigation.upload
 
 import android.content.Context
-import android.content.pm.PackageManager
-import android.net.Uri
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
-import androidx.core.app.ActivityCompat.requestPermissions
-import androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale
-import androidx.core.content.ContextCompat.checkSelfPermission
-import androidx.fragment.app.FragmentManager
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.myapplication.App
 import com.example.myapplication.R
 import com.example.myapplication.data.datasource.remote.api.RecipeDTO
-import kotlinx.android.synthetic.main.recipe_list_item.view.*
-import java.util.jar.Manifest
 
-/**
- *  기존의 어댑터에서 '갤러리' 버튼 클릭 시 itemClick 이벤트가 생기게 추가하였습니다.
- */
 class UploadRecipeAdapter(
-    private val context: Context,
-    private val recipeList: ArrayList<RecipeDTO.Recipe>,
+    val recipeList: ArrayList<RecipeDTO.Recipe>,
     val itemClick: (Int, RecipeDTO.Recipe) -> Unit
-) :
-    RecyclerView.Adapter<UploadRecipeAdapter.RecipeViewHolder>() {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(
-            R.layout.recipe_list_item,
-            parent, false
-        )
-        return RecipeViewHolder(view)
+) : RecyclerView.Adapter<UploadRecipeAdapter.UploadRecipeHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): UploadRecipeAdapter.UploadRecipeHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.recipe_list_item, parent, false)
+        return UploadRecipeHolder(view)
     }
 
     override fun getItemCount(): Int {
         return recipeList.size
     }
 
-    override fun getItemId(position: Int): Long {
-        return super.getItemId(position)
-    }
-
-    override fun getItemViewType(position: Int): Int {
-        return super.getItemViewType(position)
-    }
-
-    override fun onBindViewHolder(holder: RecipeViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: UploadRecipeAdapter.UploadRecipeHolder, position: Int) {
         val element = recipeList[position]
         holder.bind(element)
-        holder.recipeComment.setText(recipeList[position].comment)
+        holder.comment.setText(recipeList[position].comment)
 
         holder.itemView.setOnClickListener {
             Toast.makeText(
-                context,
+                App.instance,
                 "번호 : " + recipeList.get(position).number
                         + "내용 : " + recipeList.get(position).comment + "사진 : " + recipeList.get(position).image + "입니다.",
                 Toast.LENGTH_SHORT
@@ -70,44 +48,45 @@ class UploadRecipeAdapter(
         }
     }
 
-    inner class RecipeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val recipeNumber: TextView = itemView.tv_number
-        var recipeComment: EditText = itemView.et_comment
-        private val recipePhoto: ImageView = itemView.iv_photo
-        // private val recipeButton: Button = itemView.btn_gallery
+
+    inner class UploadRecipeHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val number = itemView.findViewById<TextView>(R.id.tv_number) // 단계
+        private val image = itemView.findViewById<ImageView>(R.id.iv_photo) // 레시피 사진
+        var comment = itemView.findViewById<EditText>(R.id.et_recipe_explain) // 레시피 설명
 
         fun bind(data: RecipeDTO.Recipe) {
-            recipeNumber.text = data.number
+            number.text = data.number + "번째"
 
-            if (data.image != "") {
-                Glide.with(itemView.context) //.with(itemView) -> itemView.context
+            if (data.image != null) {
+                Glide.with(itemView.context)
                     .load(data.image)
-                    .into(recipePhoto)
+                    .into(image)
             } else {
-                Glide.with(itemView.context) //.with(itemView) -> itemView.context
+                Glide.with(itemView.context)
                     .load(R.drawable.gallery)
-                    .into(recipePhoto)
+                    .into(image)
             }
 
-            recipePhoto.setOnClickListener {
+            image.setOnClickListener {
                 itemClick(adapterPosition, data)
             }
 
-            recipeComment.addTextChangedListener(object : TextWatcher{
+            comment.addTextChangedListener(object : TextWatcher {
                 override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                    Log.d("Beforeposition", adapterPosition.toString())
+                    // Log.d("Beforeposition", adapterPosition.toString())
                 }
 
                 override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                    Log.d("Onposition", adapterPosition.toString())
+                    // Log.d("Onposition", adapterPosition.toString())
                 }
 
                 override fun afterTextChanged(p0: Editable?) {
                     recipeList[adapterPosition].comment = p0.toString()
-                    Log.d("Afterposition", adapterPosition.toString())
+                    // Log.d("Afterposition", adapterPosition.toString())
                 }
             })
-
         }
+
     }
+
 }
