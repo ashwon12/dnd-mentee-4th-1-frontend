8b38f8f895021ecf48d573d9c0c2f23e2e8bf3b1
onxmoreplz <hamdoyoung95@gmail.com>
2021-02-08 23:38:01 +0900

    [Update] Data 레이어 인터페이스 삭제
    - Repository Interface 삭제 및 기존 구현클래스 이름 변경
    - LocalDataSource Interface 삭제 및 기존 구현클래스 이름 변경
    - RemoteDataSource Interface 삭제 및 기존 구현클래스 이름 변경

--- app/src/main/java/com/example/myapplication/data/repository/Repository.kt
+++ app/src/main/java/com/example/myapplication/data/repository/Repository.kt
@@ -1,19 +1,51 @@
 package com.example.myapplication.data.repository
 
+import com.example.myapplication.data.datasource.local.LocalDataSource
+import com.example.myapplication.data.datasource.remote.RemoteDataSource
 import com.example.myapplication.data.datasource.remote.api.RecipeDTO
 
-interface Repository {
+class Repository {
+
+    private val localDataSourceImpl = LocalDataSource()
+    private val remoteMovieDataSourceImpl = RemoteDataSource()
+
     fun getAllTimelineList(
         success: (RecipeDTO.PostItems) -> Unit,
         fail: (Throwable) -> Unit
-    )
+    ) {
+        remoteMovieDataSourceImpl.getAllTimelinesFromRemote(
+            success,
+            fail
+        )
+    }
+
+    fun getRandomRecipes(
+        success: (RecipeDTO.RandomRecipes) -> Unit,
+        fail: (Throwable) -> Unit
+    ) {
+        remoteMovieDataSourceImpl.getRandomRecipes(
+            success,
+            fail
+        )
+    }
+
     fun postTimeline(
         postInfo: ArrayList<RecipeDTO.PostItem>,
         success: (RecipeDTO.TimelineResponse) -> Unit,
         fail: (Throwable) -> Unit
-    )
+    ) {
+        remoteMovieDataSourceImpl.postTimeline(
+            postInfo,
+            success,
+            fail
+        )
+    }
 
-    fun saveSearch(recipe : String)
+    fun saveSearch(recipe: String) {
+        localDataSourceImpl.saveSearchWord(recipe)
+    }
 
-    fun getSavedSearchList() : List<String>
+    fun getSavedSearchList(): ArrayList<String> {
+        return localDataSourceImpl.getSavedSearchWordList()
+    }
 }
