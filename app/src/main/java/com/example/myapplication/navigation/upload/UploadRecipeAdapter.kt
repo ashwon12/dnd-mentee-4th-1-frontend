69f3cc9036cca3e70e14627d4f62e4024eba700e
parksh5061 <42669772+parksh5061@users.noreply.github.com>
2021-02-17 19:41:48 +0900

    [Update] 부분 주석 처리

--- app/src/main/java/com/example/myapplication/navigation/upload/UploadRecipeAdapter.kt
+++ app/src/main/java/com/example/myapplication/navigation/upload/UploadRecipeAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication.navigation.upload
 
+import android.net.Uri
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.LayoutInflater
@@ -34,26 +35,26 @@ class UploadRecipeAdapter(
     override fun onBindViewHolder(holder: UploadRecipeAdapter.UploadRecipeHolder, position: Int) {
         val element = recipeList[position]
         holder.bind(element)
-        holder.comment.setText(recipeList[position].comment)
+        // holder.comment.setText(recipeList[position].comment)
 
-        holder.itemView.setOnClickListener {
-            Toast.makeText(
-                App.instance,
-                "번호 : " + recipeList.get(position).number
-                        + "내용 : " + recipeList.get(position).comment + "사진 : " + recipeList.get(position).image + "입니다.",
-                Toast.LENGTH_SHORT
-            ).show()
-        }
+//        holder.itemView.setOnClickListener {
+//            Toast.makeText(
+//                App.instance,
+//                "번호 : " + recipeList.get(position).number
+//                        + "내용 : " + recipeList.get(position).comment + "사진 : " + recipeList.get(position).image + "입니다.",
+//                Toast.LENGTH_SHORT
+//            ).show()
+//        }
     }
 
 
     inner class UploadRecipeHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val number = itemView.findViewById<TextView>(R.id.tv_number) // 단계
         private val image = itemView.findViewById<ImageView>(R.id.iv_photo) // 레시피 사진
-        var comment = itemView.findViewById<EditText>(R.id.et_recipe_explain) // 레시피 설명
+        // var comment = itemView.findViewById<EditText>(R.id.et_recipe_explain) // 레시피 설명
 
         fun bind(data: RecipeDTO.Recipe) {
-            number.text = data.number + "번째"
+            number.text = data.number
 
             if (data.image != null) {
                 Glide.with(itemView.context)
@@ -61,7 +62,7 @@ class UploadRecipeAdapter(
                     .into(image)
             } else {
                 Glide.with(itemView.context)
-                    .load(R.drawable.gallery)
+                    .load("")
                     .into(image)
             }
 
@@ -69,17 +70,17 @@ class UploadRecipeAdapter(
                 itemClick(adapterPosition, data)
             }
 
-            comment.addTextChangedListener(object : TextWatcher {
-                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                }
-
-                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                }
-
-                override fun afterTextChanged(p0: Editable?) {
-                    recipeList[adapterPosition].comment = p0.toString()
-                }
-            })
+//            comment.addTextChangedListener(object : TextWatcher {
+//                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+//                }
+//
+//                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+//                }
+//
+//                override fun afterTextChanged(p0: Editable?) {
+//                    recipeList[adapterPosition].comment = p0.toString()
+//                }
+//            })
         }
 
     }
