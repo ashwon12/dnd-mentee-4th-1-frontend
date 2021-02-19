e2b1c3e3b0e5974da6b02c716fe973495032c5c3
김재원 <ashwon12@naver.com>
2021-02-20 02:44:36 +0900

    [Update] 데이터 가져오는 부분 수정

--- app/src/main/java/com/example/myapplication/navigation/feed/FeedFragment.kt
+++ app/src/main/java/com/example/myapplication/navigation/feed/FeedFragment.kt
@@ -19,62 +19,60 @@ import com.example.myapplication.R
 import com.example.myapplication.data.datasource.remote.api.RecipeDTO
 import com.example.myapplication.data.repository.Repository
 
-class FeedFragment : Fragment(), TimelineRecyclerInterface {
+class FeedFragment : Fragment(), FeedRecyclerInterface {
 
     private lateinit var v: View
-
-    private var list = ArrayList<RecipeDTO.PostItems>()
-
-    private lateinit var myAdapter: TimelineRecyclerAdapter
-
+    private var feedRecipeList =  ArrayList<RecipeDTO.tempRandomRecipes>()
+    private lateinit var myAdapter: FeedRecyclerAdapter
     private val repository = Repository()
+    private lateinit var rvFeed : RecyclerView
     
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         v = inflater.inflate(R.layout.fragment_feed, container, false)
 
-        callRecycler()
+        requsetRecipie()
         return v
     }
 
-    /**
-     *  fragment 생성시 리사이클러 호출해주는 함수
-     */
-    fun callRecycler() {
-        list.clear()
-        myAdapter = TimelineRecyclerAdapter(this)
-        var rv_list = v.findViewById(R.id.rv_list) as RecyclerView
+    private fun requsetRecipie() {
+        feedRecipeList.clear()
 
-        /**
-         * API 통신 확인을 위해 추가
-         *              - 함도영
-         * */
-        repository.getAllTimelineList(
+        feedRecipeList.add(RecipeDTO.tempRandomRecipes(0,"https://t1.daumcdn.net/cfile/tistory/9954B44D5B0AB2E22C","소고기",null,null,null,4.1,null))
+        feedRecipeList.add(RecipeDTO.tempRandomRecipes(1,"https://t1.daumcdn.net/cfile/tistory/9954B44D5B0AB2E22C","햄버거",null,null,null,4.0,null))
+        feedRecipeList.add(RecipeDTO.tempRandomRecipes(2,"https://t1.daumcdn.net/cfile/tistory/9954B44D5B0AB2E22C","감자튀김",null,null,null,4.0,null))
+        feedRecipeList.add(RecipeDTO.tempRandomRecipes(3,"https://t1.daumcdn.net/cfile/tistory/9954B44D5B0AB2E22C","레드벨벳 케이크",null,null,null,4.0,null))
+        feedRecipeList.add(RecipeDTO.tempRandomRecipes(4,"https://t1.daumcdn.net/cfile/tistory/9954B44D5B0AB2E22C","버팔로윙",null,null,null,4.0,null))
+
+        myAdapter = FeedRecyclerAdapter(this)
+        rvFeed = v.findViewById(R.id.rv_list) as RecyclerView
+
+        repository.getRandomRecipes(
             success = {
                 it.run {
-                    Log.d("it", it.toString())
-                    Log.d("it_list", listOf(it).toString())
-                    Log.d("it_get(0)", it[0].toString())
-
-                    list.add(it)
-
-                    myAdapter.updateTimelineList(it)
+                    feedRecipeList.add(it)
+                    myAdapter.feedUpdateList(feedRecipeList)
                     myAdapter.notifyDataSetChanged()
-                    rv_list.adapter = myAdapter
+                    rvFeed.adapter = myAdapter
                 }
             },
             fail = {
                 Log.d("fail", "failfailfail")
             }
         )
+        myAdapter.feedUpdateList(feedRecipeList)
+        myAdapter.notifyDataSetChanged()
+        rvFeed.adapter = myAdapter
     }
 
+
+    /** 게시글을 클릭했을 때 상제페이지로 이동 **/
     override fun onItemClicked(position: Int) {
         Log.d("로그", "TimeLinFragment - 클릭됨")
         Toast.makeText(
             App.instance,
-            "상세 값 : ${this.list[0][position]}",
+            "상세 값 : ${this.feedRecipeList[position]}",
             Toast.LENGTH_SHORT
         ).show()
     }
