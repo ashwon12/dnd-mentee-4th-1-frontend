844556fa354b5794fe7bb0f3bc3f161697b160d7
parksh5061 <42669772+parksh5061@users.noreply.github.com>
2021-02-20 23:26:09 +0900

    [Add] 인용하기 레이아웃

--- app/src/main/java/com/example/myapplication/navigation/quote/QuoteActivity.kt
+++ app/src/main/java/com/example/myapplication/navigation/quote/QuoteActivity.kt
@@ -1,43 +1,229 @@
 package com.example.myapplication.navigation.quote
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.myapplication.App
 import com.example.myapplication.R
 import com.example.myapplication.data.datasource.remote.api.RecipeDTO
+import com.example.myapplication.navigation.upload.UploadActivity3
+import com.example.myapplication.navigation.upload.UploadCommentAdapter
+import com.example.myapplication.navigation.upload.UploadSwapDelete
+import com.zhihu.matisse.Matisse
+import com.zhihu.matisse.MimeType
+import com.zhihu.matisse.engine.impl.GlideEngine
 import kotlinx.android.synthetic.main.activity_quote.*
+import kotlinx.android.synthetic.main.activity_upload3.*
 
 class QuoteActivity : AppCompatActivity() {
+    companion object {
+        private const val REQUEST_GET_IMAGE = 105
+        private const val PERMISSION_CODE = 100
+    }
+
+    private var recipeList = ArrayList<RecipeDTO.Recipe>()
+    private var commentList = ArrayList<RecipeDTO.Recipe>()
     private var filterList = ArrayList<RecipeDTO.Filter>()
     private var select_cut: Int = 0
     private var recipeTitle: String? = null
-    private var recipeList = ArrayList<RecipeDTO.Recipe>()
+    private var timeString: String = ""
     private var saveFilterList = ArrayList<String>()
     private var mainFoodTagList = ArrayList<String>()
     private var subFoodTagList = ArrayList<String>()
     private var thumbnail: Uri? = null
+    private var steps = ArrayList<RecipeDTO.Recipe>()
+    private var number = 0
+    private var number1 = 1
+    private var count = 0
+    private var positionMain = -1
+    private lateinit var itemMain: RecipeDTO.Recipe
+
+    private lateinit var imageAdapter: QuoteImageAdapter
+    private lateinit var commentAdapter: QuoteCommentAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quote)
 
         getItems()
-
+        addFoods()
+        addTags()
         callAdapter()
+        makeRecyclerView()
+        itemTouch()
+        btn_quote_add_comment.setOnClickListener {
+            addButtonClick()
+        }
+    }
+
+    private fun makeRecyclerView() {
+        for(i in 0..select_cut-1) {
+            recipeList.add(RecipeDTO.Recipe(steps[i].number, null, steps[i].image))
+            commentList.add(RecipeDTO.Recipe(steps[i].number, steps[i].comment, null))
+            number++
+            count++
+        }
+
+        if(select_cut < 9) {
+            recipeList.add(RecipeDTO.Recipe((steps.size + 1).toString(), null, null))
+            number++
+        }
     }
 
     private fun callAdapter() {
-        rv_quote_filter.layoutManager =
-            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-        rv_quote_filter.setHasFixedSize(true)
+        rv_quote_filter.apply {
+            layoutManager =
+                GridLayoutManager(App.instance, 3)
+            setHasFixedSize(true)
 
-        rv_quote_filter.adapter = QuoteFilterAdapter(filterList)
+            adapter = QuoteFilterAdapter(filterList)
+        }
+
+        var rv_recipe_list = findViewById(R.id.rv_quote_image) as RecyclerView
+        imageAdapter = QuoteImageAdapter(recipeList) { position, item ->
+            positionMain = position
+            itemMain = item
+
+            if(positionMain >= select_cut) {
+                checkPermissions()
+            } else {
+                Toast.makeText(this, "기존 레시피는 변경할 수 없습니다.", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+        imageAdapter.onItemLongClick = { position, item ->
+            positionMain = position
+            itemMain = item
+
+            if (select_cut <= positionMain) {
+                showDialog()
+            } else {
+                Toast.makeText(this, "기존 레시피는 변경할 수 없습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        rv_recipe_list.apply {
+            adapter = imageAdapter
+            layoutManager =
+                LinearLayoutManager(App.instance, LinearLayoutManager.HORIZONTAL, false)
+            setHasFixedSize(true)
+        }
+
+        commentAdapter = QuoteCommentAdapter(commentList, select_cut)
+        rv_quote_comment.adapter = commentAdapter
+        rv_quote_comment.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        rv_quote_comment.setHasFixedSize(true)
     }
 
+    private fun addTags() {
+        filterList.clear()
+
+        saveFilterList.add(select_cut.toString() + "컷")
+
+        if (Integer.parseInt(timeString) == 59) {
+            saveFilterList.add("60분 이내")
+        } else if (Integer.parseInt(timeString) >= 60) {
+            saveFilterList.add("60분 이상")
+        } else {
+            saveFilterList.add(timeString + "분 이내")
+        }
+
+        for (i in saveFilterList.indices) {
+            filterList.add(RecipeDTO.Filter(saveFilterList[i]))
+        }
+    }
+
+    private fun addFoods() {
+        var food = ""
+
+        for (i in mainFoodTagList.indices) {
+            food += mainFoodTagList[i] + ", "
+        }
+        for (i in subFoodTagList.indices) {
+            if (i == subFoodTagList.size - 1) {
+                food += subFoodTagList[i]
+            } else {
+                food += subFoodTagList[i] + ", "
+            }
+        }
+
+        tv_quote_recipe_food.text = food
+
+    }
+
+    private fun addRecyclerView() {
+        if (number < 9 && number > 3) {
+            number++
+            recipeList.add(RecipeDTO.Recipe(number.toString(), null, null))
+        }
+       //  Log.d("RecyclerView number111", number1.toString())
+        Log.d("RecyclerView number", number.toString())
+       //  Log.d("recipeList", recipeList.toString())
+    }
+
+    private fun addButtonClick() {
+        if (count < 9) {
+            addItem(
+                rv_quote_comment.adapter!!.itemCount,
+                RecipeDTO.Recipe(Integer.toString(count + 1), "", "")
+            )
+            addRecyclerView()
+            imageAdapter.notifyDataSetChanged()
+
+        } else {
+            Toast.makeText(this, "9개 이상 추가할 수 없습니다.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun itemTouch() {
+        val item = object : UploadSwapDelete(this, 0, ItemTouchHelper.LEFT) {
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                if(viewHolder.adapterPosition >= select_cut) {
+                    commentAdapter.deleteItem(viewHolder.adapterPosition)
+                    Log.d("id", viewHolder.itemId.toString())
+                    Log.d("position", viewHolder.adapterPosition.toString())
+                    Log.d("direction", direction.toString())
+                    Log.d("positionMain", positionMain.toString())
+                    Log.d("count", count.toString())
+                    imageAdapter.deleteItem(viewHolder.layoutPosition)
+                    number--
+                    count--
+                    if (viewHolder.layoutPosition == 8) {
+                        addRecyclerView()
+                    }
+                } else {
+                    Toast.makeText(App.instance, "기존 레시피는 변경할 수 없습니다.", Toast.LENGTH_SHORT).show()
+                    commentAdapter.notifyDataSetChanged()
+                }
+
+            }
+        }
+
+        val itemTouchHelper = ItemTouchHelper(item)
+        itemTouchHelper.attachToRecyclerView(rv_quote_comment)
+    }
+
+    private fun addItem(position: Int, data: RecipeDTO.Recipe) {
+        if (count < 9) {
+            commentList.add(position, data)
+            commentAdapter.notifyDataSetChanged()
+            count++
+        }
+    }
     private fun getItems() {
         if (intent.hasExtra("number")) {
             select_cut = intent.getIntExtra("number", 1)
@@ -45,9 +231,6 @@ class QuoteActivity : AppCompatActivity() {
         if (intent.hasExtra("filter")) {
             saveFilterList = intent.getStringArrayListExtra("filter")!!
             Log.d("savefilterList", saveFilterList.toString())
-        }
-        if (intent.hasExtra("originFilter")) {
-            filterList = intent.getSerializableExtra("originFilter") as ArrayList<RecipeDTO.Filter>
         }
         if (intent.hasExtra("thumbnail")) {
             thumbnail = intent.getParcelableExtra("thumbnail")
@@ -62,12 +245,17 @@ class QuoteActivity : AppCompatActivity() {
             subFoodTagList = intent.getStringArrayListExtra("subfood")!!
             Log.d("subfood", subFoodTagList.toString())
         }
-        if (intent.hasExtra("recipeList")) {
-            recipeList = intent.getSerializableExtra("recipeList") as ArrayList<RecipeDTO.Recipe>
-        }
         if (intent.hasExtra("recipeTitle")) {
             recipeTitle = intent.getStringExtra("recipeTitle")
             tv_quote_recipe_title.text = recipeTitle
+        }
+        if (intent.hasExtra("steps")) {
+            steps = intent.getSerializableExtra("steps") as ArrayList<RecipeDTO.Recipe>
+            Log.d("steps", steps.toString())
+        }
+        if (intent.hasExtra("time")) {
+            timeString = intent.getStringExtra("time")!!
+            Log.d("time", timeString)
         }
     }
 
@@ -75,5 +263,152 @@ class QuoteActivity : AppCompatActivity() {
         Glide.with(this)
             .load(thumbnail)
             .into(iv_quote_recipe_poster)
+    }
+
+    private fun pickUpGallery() {
+        Matisse.from(this)
+            .choose(MimeType.ofAll())
+            .countable(true)
+            .maxSelectable(9)
+            .spanCount(3)
+            .imageEngine(GlideEngine())
+            .forResult(REQUEST_GET_IMAGE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == RESULT_OK) {
+            when (requestCode) {
+                REQUEST_GET_IMAGE ->
+                    Matisse.obtainResult(data)?.let {
+                        val mSelected: List<Uri> = Matisse.obtainResult(data)
+                        val size = mSelected.size
+                        Log.d("positionMain", positionMain.toString())
+                        Log.d("size", size.toString())
+                        Log.d("inner number", number.toString())
+                        if (size == 1) {
+                            recipeList[positionMain].image = mSelected[0].toString()
+                            addRecyclerView()
+                            Log.d("number1", number.toString())
+                            if (number > 3 && positionMain >= number - 2) {
+                                addItem(
+                                    rv_quote_comment.adapter!!.itemCount,
+                                    RecipeDTO.Recipe(Integer.toString(count + 1), "", "")
+                                )
+                            }
+                            imageAdapter.notifyDataSetChanged()
+                        } else if (size <= 9) {
+                            if (positionMain + size <= 9) {
+                                if(positionMain + size == number) {
+                                    for (i in mSelected.indices) {
+                                        recipeList[positionMain + i].image = mSelected[i].toString()
+                                        imageAdapter.notifyDataSetChanged()
+                                    }
+                                    addRecyclerView()
+                                    addItem(
+                                        rv_quote_comment.adapter!!.itemCount,
+                                        RecipeDTO.Recipe(Integer.toString(count + 1), "", "")
+                                    )
+                                } else if (positionMain + size <= number - 1) {
+                                    for (i in mSelected.indices) {
+                                        recipeList[positionMain + i].image = mSelected[i].toString()
+                                        imageAdapter.notifyDataSetChanged()
+                                        Log.d("here!!", positionMain.toString())
+                                    }
+                                } else {
+                                    for (i in mSelected.indices) {
+                                        recipeList[positionMain + i].image = mSelected[i].toString()
+                                        addRecyclerView()
+
+                                        Log.d("number3", number.toString())
+                                        if(number > 4) {
+                                            addItem(
+                                                rv_quote_comment.adapter!!.itemCount,
+                                                RecipeDTO.Recipe(Integer.toString(count + 1), "", "")
+                                            )
+                                        }
+
+                                        imageAdapter.notifyDataSetChanged()
+                                    }
+                                }
+
+                            } else if (number >= 9) {
+                                Toast.makeText(
+                                    App.instance,
+                                    "사진을 9개 이상 전송할 수 없습니다.",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+
+                        } else {
+                            Toast.makeText(
+                                App.instance,
+                                "사진을 9개 이상 전송할 수 없습니다.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                        Log.d("number2", number.toString())
+                    }
+                else -> finish()
+            }
+        }
+    }
+
+    private fun checkPermissions() {
+        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+            val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+            requestPermissions(permissions, PERMISSION_CODE)
+        } else {
+            pickUpGallery()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            PERMISSION_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    pickUpGallery()
+                } else {
+                    Toast.makeText(App.instance, "권한이 없습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun showDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage("사진을 삭제하시겠습니까?")
+            .setPositiveButton("확인", DialogInterface.OnClickListener { dialog, which ->
+                Log.d("positionMain", positionMain.toString())
+                Log.d("count", count.toString())
+                if ((positionMain < count) && count > 3) {
+                    imageAdapter.deleteItem(positionMain)
+                    commentAdapter.deleteItem(positionMain)
+                    if(positionMain == 4) {
+                        addRecyclerView()
+                    }
+                    count--
+                    number--
+                    if (positionMain == 8) {
+                        addRecyclerView()
+                    }
+                } else {
+                    if(count <= 3){
+                        Toast.makeText(this, "사진은 최소 3장 업로드 해야합니다.\n사진을 눌러 변경해주세요.", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "삭제하실 수 없습니다.", Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+                Log.d("dialog_number", number.toString())
+            })
+            .setNegativeButton("취소", DialogInterface.OnClickListener { dialog, which ->
+            })
+        builder.show()
     }
 }
