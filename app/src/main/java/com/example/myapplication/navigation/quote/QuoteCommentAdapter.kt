1f2682887f5c7d56f3af959f9d1d078328c933b5
parksh5061 <42669772+parksh5061@users.noreply.github.com>
2021-02-21 04:29:11 +0900

    [Update] 인용하기 설명 어댑터

--- app/src/main/java/com/example/myapplication/navigation/quote/QuoteCommentAdapter.kt
+++ app/src/main/java/com/example/myapplication/navigation/quote/QuoteCommentAdapter.kt
@@ -2,6 +2,7 @@ package com.example.myapplication.navigation.quote
 
 import android.graphics.Color
 import android.text.*
+import android.text.InputFilter.LengthFilter
 import android.text.style.ForegroundColorSpan
 import android.util.Log
 import android.view.LayoutInflater
@@ -13,8 +14,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
 import com.example.myapplication.data.datasource.remote.api.RecipeDTO
 
+
 class QuoteCommentAdapter(
     val commentList: ArrayList<RecipeDTO.Recipe>,
+    val saveCommentList: ArrayList<RecipeDTO.Recipe>,
     val select_cut: Int
 ) : RecyclerView.Adapter<QuoteCommentAdapter.QuoteCommentHolder>() {
     override fun onCreateViewHolder(
@@ -22,7 +25,7 @@ class QuoteCommentAdapter(
         viewType: Int
     ): QuoteCommentAdapter.QuoteCommentHolder {
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.upload_comment_list_item, parent, false)
+            .inflate(R.layout.quote_comment_list_item, parent, false)
         return QuoteCommentHolder(view)
     }
 
@@ -59,13 +62,29 @@ class QuoteCommentAdapter(
     inner class QuoteCommentHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private var number = itemView.findViewById<TextView>(R.id.tv_upload_number) // 단계
         var comment = itemView.findViewById<EditText>(R.id.et_upload_comment) // 레시피 설명
+        var len = itemView.findViewById<TextView>(R.id.tv_quote_comment_length)
 
         fun bind(data: RecipeDTO.Recipe) {
             number.text = data.number
-            val saveComment = commentList[adapterPosition].comment
-            val wordToSpan = SpannableString(saveComment)
-            var count = true
-            wordToSpan.setSpan(ForegroundColorSpan(Color.parseColor("#464646")) ,0,wordToSpan.length,Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+            // var saveComment = commentList[adapterPosition].comment
+            var saveComment = commentList[adapterPosition].comment
+            var input = commentList[adapterPosition].comment?.substring(saveComment!!.length)
+            var wordToSpan = SpannableString(saveComment)
+            Log.d("wordToSpan", wordToSpan.toString())
+
+            Log.d("input", input.toString())
+            Log.d("commentList", commentList.toString())
+            wordToSpan.setSpan(
+                ForegroundColorSpan(Color.parseColor("#464646")),
+                0,
+                wordToSpan.length,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+            comment.setFilters(arrayOf<InputFilter>(LengthFilter(100 + saveComment!!.length)))
+
+//            Log.d("saveComment", saveComment.toString())
+//            Log.d("saveComment len", saveComment!!.length.toString())
             comment.addTextChangedListener(object : TextWatcher {
                 override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 }
@@ -74,19 +93,42 @@ class QuoteCommentAdapter(
                 }
 
                 override fun afterTextChanged(p0: Editable?) {
-                    if(!p0.toString().startsWith(saveComment.toString()) || count == true) {
-                        count = false
-                        comment!!.setText(wordToSpan)
-                        Selection.setSelection(comment.text,comment.length())
+                    // saveComment = commentList[adapterPosition].comment
+                    commentList[adapterPosition].comment = p0?.toString()
+                    Log.d("saveComment", saveComment.toString())
+                    Log.d("saveComment len", saveComment!!.length.toString())
+                    input = commentList[adapterPosition].comment?.substring(saveComment!!.length)
+                    Log.d("input", input.toString())
+
+                    if (input?.length!! > 0) {
+                        number.setBackgroundResource(R.drawable.ic_select_oval)
                     } else {
-                        commentList[adapterPosition].comment = p0.toString()
+                        number.setBackgroundResource(R.drawable.ic_oval)
+                    }
+                    if (select_cut > adapterPosition) {
+                        if (!p0.toString().startsWith(saveComment.toString())) {
+                            comment!!.setText(wordToSpan)
+                            Selection.setSelection(comment.text, comment.length())
+                            comment.setTextColor(Color.parseColor("#FF7051"))
+                        }
+                    } else {
+                        // commentList[adapterPosition].comment = p0.toString()
                         comment.setTextColor(Color.parseColor("#FF7051"))
                     }
+                    // saveComment = commentList[adapterPosition].comment
+                    // input = commentList[adapterPosition].comment?.substring(saveComment!!.length)
+                    // commentList[adapterPosition].comment = p0.toString()
+                    len.setText("글자수 : " + input?.length.toString() + "/100")
+//                    Log.d("input len", input?.length.toString())
+//                    Log.d("input value", input.toString())
 
-                    Log.d("p0", p0.toString())
+                    Log.d("commentList", commentList[adapterPosition].comment.toString())
                 }
             })
+
+
         }
+
     }
 
 }
