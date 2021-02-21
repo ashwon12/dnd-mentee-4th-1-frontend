2df785256d6d57d1d7bb3151cdf41118fdee7928
onxmoreplz <hamdoyoung95@gmail.com>
2021-02-22 01:23:16 +0900

    [Add] StatusBar 투명하게 만들기

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.myapplication
 
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import com.example.myapplication.navigation.feed.FeedFragment
 import androidx.fragment.app.FragmentManager
+import com.example.myapplication.navigation.feed.FeedFragment
 import com.example.myapplication.navigation.home.HomeFragment
 import com.example.myapplication.navigation.mypage.MyPageFragment
 import com.example.myapplication.navigation.search.SearchFragment
 import com.example.myapplication.navigation.upload.UploadActivity
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,11 +24,42 @@ class MainActivity : AppCompatActivity() {
     private val searchFragment by lazy { SearchFragment() }
     private val myPageFragment by lazy { MyPageFragment() }
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setStatusBarTransparent()
+
+
         initBottomNavigation()
+
+
+
+    }
+
+    fun setStatusBarTransparent() {
+        if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
+            setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true)
+        }
+        if (Build.VERSION.SDK_INT >= 19) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        }
+        if (Build.VERSION.SDK_INT >= 21) {
+            setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
+            window.statusBarColor = Color.TRANSPARENT
+        }
+    }
+
+    fun setWindowFlag(bits: Int, on: Boolean) {
+        val win = window
+        val winParams = win.attributes
+        if (on) {
+            winParams.flags = winParams.flags or bits
+        } else {
+            winParams.flags = winParams.flags and bits.inv()
+        }
+        win.attributes = winParams
     }
 
     /**
@@ -36,16 +72,16 @@ class MainActivity : AppCompatActivity() {
         bnv_home.run {
             setOnNavigationItemSelectedListener {
                 when(it.itemId){
-                    R.id.home ->{
+                    R.id.home -> {
                         setFragment(homeFragment)
                     }
                     R.id.feed -> {
                         setFragment(feedFragment)
                     }
-                    R.id.search->{
+                    R.id.search -> {
                         setFragment(searchFragment)
                     }
-                    R.id.myPage->{
+                    R.id.myPage -> {
                         setFragment(myPageFragment)
                     }
                 }
