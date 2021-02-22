21f9090bf08f00f75e1f3335271476506721f0d1
김재원 <ashwon12@naver.com>
2021-02-23 04:50:45 +0900

    [Update] 상세페이지로 이동

--- app/src/main/java/com/example/myapplication/navigation/home/MultiViewAdapter.kt
+++ app/src/main/java/com/example/myapplication/navigation/home/MultiViewAdapter.kt
@@ -1,28 +1,35 @@
 package com.example.myapplication.navigation.home
 
-import android.media.Image
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.myapplication.App
 import com.example.myapplication.R
 import com.example.myapplication.data.datasource.remote.api.RecipeDTO
+import com.example.myapplication.detail.DetailFragment
 
 class MultiViewAdapter(
+
     private var type: Int,
-    private var Images: ArrayList<RecipeDTO.RecipeFinal>
+    private var ItemsList: ArrayList<RecipeDTO.RecipeFinal>
 ) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    lateinit var view : View
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): RecyclerView.ViewHolder {
-        val view: View?
         return when (viewType) {
             1 -> {
                 view = LayoutInflater.from(parent.context)
@@ -44,7 +51,7 @@ class MultiViewAdapter(
     }
 
     override fun getItemCount(): Int {
-        return Images.size
+        return ItemsList.size
     }
 
     override fun getItemViewType(position: Int): Int {
@@ -54,37 +61,56 @@ class MultiViewAdapter(
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         when (type) {
             1 -> {
-                for(i in 0 .. 2) {
-                    Glide.with(App.instance)
-                        .load(Images[i].thumbnail)
-                        .placeholder(R.drawable.ic_no_image)
-                        .into((holder as ViewPagerViewHolder).top3Image)
-                }
+                Glide.with(App.instance)
+                    .load(ItemsList[position].thumbnail)
+                    .placeholder(R.drawable.ic_no_image)
+                    .into((holder as ViewPagerViewHolder).top3Image)
             }
 
             2 -> {
-                for(i in 0 .. 4){
+                for (i in 0..4) {
                     Glide.with(App.instance)
-                        .load(Images[i].thumbnail)
+                        .load(ItemsList[i].thumbnail)
                         .placeholder(R.drawable.ic_no_image)
                         .into((holder as GridViewHolder).popularImage)
                 }
             }
 
             3 -> {
-                val mainIngredients = Images[position].mainIngredients
+                val mainIngredients = ItemsList[position].mainIngredients
                 val sb = StringBuilder()
-                for (text in mainIngredients){
-                    sb.append(text.name+"\t")
+                for (text in mainIngredients) {
+                    sb.append(text.name + "\t")
                 }
                 sb.toString()
                 (holder as ListViewHolder).recentContent.text = sb
-                holder.recentTitle.text = Images[position].title
+                holder.recentTitle.text = ItemsList[position].title
                 Glide.with(App.instance)
-                    .load(Images[position].thumbnail)
+                    .load(ItemsList[position].thumbnail)
                     .placeholder(R.drawable.ic_no_image)
                     .into(holder.recentImage)
             }
+        }
+
+        holder.itemView.setOnClickListener {
+            Toast.makeText(
+                App.instance,
+                "ID : ${this.ItemsList[position].id} " +
+                        "Title : ${this.ItemsList[position].title}",
+                Toast.LENGTH_SHORT
+            ).show()
+
+
+            val activity = view.context as AppCompatActivity
+            val detailFragment: Fragment = DetailFragment()
+
+            val manager: FragmentManager = activity.supportFragmentManager
+            manager.beginTransaction()
+                .setCustomAnimations(R.anim.slide_in, R.anim.fade_out, R.anim.fade_in, R.anim.slide_out)
+                .setReorderingAllowed(true)
+                .replace(R.id.fl_container, detailFragment)
+                .addToBackStack(null)
+                .commit()
         }
     }
 
@@ -97,9 +123,9 @@ class MultiViewAdapter(
     }
 
     inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val recentImage : ImageView = itemView.findViewById(R.id.iv_recent_item)
-        val recentTitle : TextView = itemView.findViewById(R.id.tv_home_title)
-        val recentContent : TextView = itemView.findViewById(R.id.tv_home_content)
+        val recentImage: ImageView = itemView.findViewById(R.id.iv_recent_item)
+        val recentTitle: TextView = itemView.findViewById(R.id.tv_home_title)
+        val recentContent: TextView = itemView.findViewById(R.id.tv_home_content)
     }
 }
 
