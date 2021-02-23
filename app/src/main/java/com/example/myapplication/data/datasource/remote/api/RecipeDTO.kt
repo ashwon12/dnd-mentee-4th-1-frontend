cb6423169252cdea9257ff4a39073e50add0bb3f
parksh5061 <42669772+parksh5061@users.noreply.github.com>
2021-02-23 20:26:57 +0900

    [Update] 이미지 s3 업로드 dto 추가

--- app/src/main/java/com/example/myapplication/data/datasource/remote/api/RecipeDTO.kt
+++ app/src/main/java/com/example/myapplication/data/datasource/remote/api/RecipeDTO.kt
@@ -3,7 +3,9 @@
  */
 package com.example.myapplication.data.datasource.remote.api
 
+import android.net.Uri
 import java.io.Serializable
+import java.sql.Timestamp
 
 class RecipeDTO {
 
@@ -49,23 +51,33 @@ class RecipeDTO {
         val wishCount: Int?
 //        val writer: User
     )
-  
+
+    data class UploadImage(
+        val timestamp: String?,
+        val status: String?,
+        val error: String?,
+        val message: String?,
+        val path: String?,
+        var data: String?
+    )
+
     data class Timeline(
         val id: String,
         val title: String,
         val subTitle: String,
         val images: List<Recipe>? = null
     )
-  
+
     data class Recipe(
         var number: String?,
         var comment: String?,
         var image: String?
     ) : Serializable
 
-    data class Filter(
-        var filterName: String
-    ) : Serializable
+//    data class Filter(
+//        var id: String,
+//        var filterName: String
+//    ) : Serializable
 
     data class Time(
         var timeName: String
@@ -79,18 +91,19 @@ class RecipeDTO {
         val comment: String?
     )
 
-// -------------- 2020. 2. 21 추가 -------------
+    // -------------- 2020. 2. 21 추가 -------------
     data class RecipeFinal(
         var themes: Array<Themes>,
         var thumbnail: String? = null,
         var starCount: String? = null,
         var mainIngredients: Array<MainIngredients>,
-        var subIngredients: Array<String>,
+        var subIngredients: Array<SubIngredients>,
         var id: String? = null,
         var time: String? = null,
         var viewCount: String? = null,
         var writer: Writer? = null,
         var title: String? = null,
+        var subtitle: String? = null,
         var wishCount: String? = null,
         var steps: Array<Steps>
     )
@@ -109,11 +122,15 @@ class RecipeDTO {
     )
 
     class Themes(
-        var name: String? = null,
-        var id: String? = null
-    )
+        var id: String? = null,
+        var name: String? = null
+    ) : Serializable
 
     class MainIngredients {
+        var name: String? = null
+    }
+
+    class SubIngredients {
         var name: String? = null
     }
 }
