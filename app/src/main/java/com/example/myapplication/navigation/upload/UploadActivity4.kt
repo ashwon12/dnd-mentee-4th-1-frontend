aa7bf2ea144bc7dc377e48d87a78599b976f8e10
parksh5061 <42669772+parksh5061@users.noreply.github.com>
2021-02-24 15:32:38 +0900

    [Add] DTO 정리, 레시피 등록 함수 추가

--- app/src/main/java/com/example/myapplication/navigation/upload/UploadActivity4.kt
+++ app/src/main/java/com/example/myapplication/navigation/upload/UploadActivity4.kt
@@ -17,14 +17,15 @@ import com.example.myapplication.R
 import com.example.myapplication.data.datasource.remote.api.RecipeDTO
 import com.example.myapplication.data.repository.Repository
 import com.example.myapplication.navigation.quote.QuoteActivity
-import kotlinx.android.synthetic.main.activity_upload2.*
 import kotlinx.android.synthetic.main.activity_upload4.*
-import kotlinx.android.synthetic.main.activity_upload4.iv_upload_cancel
+import org.json.JSONObject
 
 class UploadActivity4 : AppCompatActivity() {
-    private var select_cut: Int = 0
-    private var recipeTitle : String? = null
-    private var subTitle : String? = null
+    private var data = HashMap<String, Any>()
+    private var themes = ArrayList<RecipeDTO.Themes>()
+    private var select_cut: String? = null
+    private var recipeTitle: String? = null
+    private var subTitle: String? = null
     private var steps = ArrayList<RecipeDTO.Recipe>()
     private var timeString: String = ""
     private var saveFilterList = ArrayList<String>()
@@ -33,9 +34,17 @@ class UploadActivity4 : AppCompatActivity() {
     private var thumbnail: String? = null
     private var filterList = ArrayList<RecipeDTO.Themes>()
     private lateinit var adapter: UploadPreviewRecipeAdapter
+    private var obj = JSONObject()
+    val recipeResult = RecipeDTO.UploadRecipe(null, null, null, ArrayList<RecipeDTO.MainIngredients>(),ArrayList<RecipeDTO.SubIngredients>(), ArrayList<Int>(), ArrayList<RecipeDTO.Steps>(),null,null,null,null)
+    val recipePostResult = RecipeDTO.APIResponseData(
+        null, null, null, null, null,
+        RecipeDTO.RecipeFinal(null, "", "", "",
+            ArrayList<RecipeDTO.MainIngredients>(),ArrayList<RecipeDTO.SubIngredients>(), null, null, null, "", "", "", null, null)
+    )
+    // private lateinit var recipePostResult : RecipeDTO.APIResponseData
 
-    private lateinit var recipePostResult : RecipeDTO.RecipeFinal
     private val repository = Repository()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,7 +67,7 @@ class UploadActivity4 : AppCompatActivity() {
     }
 
     private fun clickPrevButton() {
-        val intent = Intent(this, QuoteActivity::class.java )
+        val intent = Intent(this, QuoteActivity::class.java)
         intent.putExtra("number", select_cut)
         intent.putExtra("filter", saveFilterList)
         intent.putExtra("thumbnail", thumbnail)
@@ -67,14 +76,15 @@ class UploadActivity4 : AppCompatActivity() {
         intent.putExtra("steps", steps)
         intent.putExtra("recipeTitle", recipeTitle)
         intent.putExtra("time", timeString)
-        intent.putExtra("subtitle",subTitle)
+        intent.putExtra("subtitle", subTitle)
         startActivity(intent)
         finish()
     }
 
     private fun clickSubmitButton() {
-
-        val intent = Intent(this, MainActivity::class.java )
+        setResult()
+        
+        val intent = Intent(this, MainActivity::class.java)
         intent.putExtra("number", select_cut)
         intent.putExtra("filter", saveFilterList)
         intent.putExtra("thumbnail", thumbnail)
@@ -83,15 +93,17 @@ class UploadActivity4 : AppCompatActivity() {
         intent.putExtra("steps", steps)
         intent.putExtra("recipeTitle", recipeTitle)
         intent.putExtra("time", timeString)
-        intent.putExtra("subtitle",subTitle)
+        intent.putExtra("subtitle", subTitle)
         intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
         startActivity(intent)
     }
 
     private fun clickCancelButton() {
         val builder = AlertDialog.Builder(this)
-        builder.setMessage("나중에 올릴 땐 다시 작성해야해요\n" +
-                "작성을 멈추시겠어요?")
+        builder.setMessage(
+            "나중에 올릴 땐 다시 작성해야해요\n" +
+                    "작성을 멈추시겠어요?"
+        )
             .setPositiveButton("확인", DialogInterface.OnClickListener { dialog, which ->
                 val intent = Intent(this, MainActivity::class.java)
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
@@ -106,46 +118,64 @@ class UploadActivity4 : AppCompatActivity() {
         steps.clear()
 
         if (intent.hasExtra("number")) {
-            // select_cut = intent.getIntExtra("number", 1)
-            recipePostResult.viewCount = intent.getIntExtra("number", 1).toString()
-            Log.d("select_cut", recipePostResult.viewCount.toString())
+            select_cut = intent.getStringExtra("number")
         }
-        if (intent.hasExtra("filter")) {
-            // saveFilterList = intent.getStringArrayListExtra("filter")!!
-            recipePostResult.themes = intent.getSerializableExtra("filter") as Array<RecipeDTO.Themes>
-            Log.d("savefilterList", recipePostResult.themes.toString())
+        if (intent.hasExtra("themes")) {
+            themes = intent.getSerializableExtra("themes") as ArrayList<RecipeDTO.Themes>
         }
         if (intent.hasExtra("thumbnail")) {
             thumbnail = intent.getStringExtra("thumbnail")
-            Log.d("thumbnail", thumbnail.toString())
         }
         if (intent.hasExtra("mainfood")) {
             mainFoodTagList = intent.getStringArrayListExtra("mainfood")!!
-            Log.d("mainfood", mainFoodTagList.toString())
         }
         if (intent.hasExtra("subfood")) {
             subFoodTagList = intent.getStringArrayListExtra("subfood")!!
-            Log.d("subfood", subFoodTagList.toString())
         }
         if (intent.hasExtra("steps")) {
             steps = intent.getSerializableExtra("steps") as ArrayList<RecipeDTO.Recipe>
-            setPageImage()
-            Log.d("steps", steps.toString())
         }
         if (intent.hasExtra("recipeTitle")) {
             recipeTitle = intent.getStringExtra("recipeTitle")
-            Log.d("recipeTitle", recipeTitle.toString())
         }
-        if(intent.hasExtra("time")) {
+        if (intent.hasExtra("time")) {
             timeString = intent.getStringExtra("time")!!
-            Log.d("time", timeString)
         }
         if (intent.hasExtra("subtitle")) {
             subTitle = intent.getStringExtra("subtitle")!!
-            Log.d("subTitle", subTitle.toString())
+        }
+        if (intent.hasExtra("recipeTitle")) {
+            recipeTitle = intent.getStringExtra("recipeTitle")
         }
     }
 
+    private fun setResult() {
+        recipeResult.title = recipeTitle.toString()
+        recipeResult.description = subTitle.toString()
+        recipeResult.thumbnail = thumbnail.toString()
+        recipeResult.writerId = 1
+        recipeResult.time = timeString
+        recipeResult.viewCount = select_cut
+
+        for (i in mainFoodTagList.indices) {
+            recipeResult.mainIngredients?.add(RecipeDTO.MainIngredients(mainFoodTagList[i]))
+        }
+
+        for (i in subFoodTagList.indices) {
+            recipeResult.subIngredients?.add(RecipeDTO.SubIngredients(subFoodTagList[i]))
+        }
+
+        for(i in steps.indices) {
+            recipeResult.steps?.add(RecipeDTO.Steps(null, steps[i].comment, steps[i].image, null))
+        }
+
+        for(i in themes.indices) {
+            recipeResult.themeIds?.add(themes[i].id!!)
+        }
+        recipeResult.pid = 1
+
+        recipeUpload()
+    }
 
     private fun setPageImage() {
         var rv_recipe_list = findViewById(R.id.rv_upload_preview_recipe) as RecyclerView
@@ -156,21 +186,14 @@ class UploadActivity4 : AppCompatActivity() {
         rv_recipe_list.setHasFixedSize(true)
     }
 
-    private fun postRecipeUpload() {
-        repository.postRecipeUpload(recipePostResult, success = {
-
-        }, fail = {
-
-        })
-    }
-
     /**  StatusBar 투명하게 만들기(1)  **/
     fun setStatusBarTransparent() {
         if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
             setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true)
         }
         if (Build.VERSION.SDK_INT >= 19) {
-            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            window.decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
         }
         if (Build.VERSION.SDK_INT >= 21) {
             setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
@@ -188,5 +211,14 @@ class UploadActivity4 : AppCompatActivity() {
             winParams.flags = winParams.flags and bits.inv()
         }
         win.attributes = winParams
+    }
+
+    private fun recipeUpload() {
+        repository.postRecipeUpload(recipeResult,
+            success = {
+                Log.d("success", "success")
+            }, fail = {
+                Log.d("function fail", "fail")
+            })
     }
 }
