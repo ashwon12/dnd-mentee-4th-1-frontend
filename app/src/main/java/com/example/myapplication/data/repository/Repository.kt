73bdb9d0fc9f7f0a0fd600ac4b0b6a494bc1545a
parksh5061 <42669772+parksh5061@users.noreply.github.com>
2021-02-25 02:52:40 +0900

    [Update] 로그인, 회원가입 함수 추가

--- app/src/main/java/com/example/myapplication/data/repository/Repository.kt
+++ app/src/main/java/com/example/myapplication/data/repository/Repository.kt
@@ -71,5 +71,42 @@ class Repository {
         return localDataSourceImpl.getSavedSearchWordList()
     }
 
+    // 이미지 s3 업로드
 
+    fun postImageUpload(
+        imagePath: String,
+        success: (RecipeDTO.UploadImage) -> Unit,
+        fail: (Throwable) -> Unit
+    ) {
+        remoteMovieDataSourceImpl.postImageUpload(imagePath, success, fail)
+    }
+
+    // 레시피 등록
+
+    fun postRecipeUpload(
+        recipeInfo: (RecipeDTO.UploadRecipe),
+
+        success: (RecipeDTO.UploadRecipe) -> Unit,
+        fail: (Throwable) -> Unit
+    ) {
+        remoteMovieDataSourceImpl.postRecipeUpload(recipeInfo, success, fail)
+    }
+
+    fun postLoginInfo(
+        token: String,
+        email: RecipeDTO.RequestPostLogin,
+        success : (RecipeDTO.RequestPostLogin) -> Unit,
+        fail : (Throwable) -> Unit
+    ) {
+        remoteMovieDataSourceImpl.postLoginInfo(token, email, success, fail)
+    }
+
+    fun postJoinInfo(
+        token: String,
+        joinInfo : RecipeDTO.RequestJoin,
+        success: (RecipeDTO.RequestJoin) -> Unit,
+        fail : (Throwable) -> Unit
+    ) {
+        remoteMovieDataSourceImpl.postJoinInfo(token, joinInfo, success, fail)
+    }
 }
