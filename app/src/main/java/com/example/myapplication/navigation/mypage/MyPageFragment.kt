8437dbfc144862516d664820d71685298b4eb23f
김재원 <ashwon12@naver.com>
2021-02-26 04:15:25 +0900

    [Update] mypage 유저정보 파싱

--- app/src/main/java/com/example/myapplication/navigation/mypage/MyPageFragment.kt
+++ app/src/main/java/com/example/myapplication/navigation/mypage/MyPageFragment.kt
@@ -3,38 +3,53 @@ package com.example.myapplication.navigation.mypage
 import MyPageFollowerFragment
 import MyPageFollowingFragment
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.example.myapplication.App
 import com.example.myapplication.R
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.kakao.sdk.user.UserApiClient
 import kotlinx.android.synthetic.main.fragment_mypage.*
+import com.example.myapplication.SharedPreferenceUtil as SharedPreferenceUtil1
 
 
 class MyPageFragment : Fragment() {
+
+    private lateinit var v : View
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        v =  inflater.inflate(R.layout.fragment_mypage, container, false)
 
-
-        return inflater.inflate(R.layout.fragment_mypage, container, false)
+        return v
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        updateKaKaoLoginUi()
-        updateGoogleLoginUi()
+
         initTabs()
         settingButton()
+        setUser()
+    }
+
+    private fun setUser() {
+        val profile = v.findViewById<ImageView>(R.id.iv_user_profile)
+        val userName = v.findViewById<TextView>(R.id.tv_user_id)
+        val imageUrl = SharedPreferenceUtil1(App.instance).getImage()
+
+        Glide.with(App.instance)
+            .load(imageUrl)
+            .circleCrop()
+            .placeholder(R.drawable.ic_no_image)
+            .into(profile)
+
+        userName.text = SharedPreferenceUtil1(App.instance).getName()
     }
 
     private fun settingButton() {
@@ -57,46 +72,4 @@ class MyPageFragment : Fragment() {
         mypage_tablayout.setupWithViewPager(mypage_viewpager)
     }
 
-    private fun updateKaKaoLoginUi() {
-        UserApiClient.instance.me { user, error ->
-            if (error != null) {
-                Log.e("kakao", "사용자 정보 요청 실패", error)
-            } else if (user != null) {
-                Log.i(
-                    "kakao", "사용자 정보 요청 성공" +
-                            "\n회원번호: ${user.id}" +
-                            "\n닉네임: ${user.kakaoAccount?.profile?.nickname}" +
-                            "\n프로필사진: ${user.kakaoAccount?.profile?.profileImageUrl}"
-                )
-                tv_user_id.setText(user.kakaoAccount?.profile?.nickname)
-                Glide.with(App.instance)
-                    .load(user.kakaoAccount?.profile?.profileImageUrl)
-                    .into(iv_user_profile)
-            }
-        }
-    }
-
-    private fun updateGoogleLoginUi() {
-        val acct = GoogleSignIn.getLastSignedInAccount(App.instance)
-
-        if (acct != null) {
-            val personName = acct?.displayName
-            val personGivenName = acct?.givenName
-            val personFamilyName = acct?.familyName
-            val personEmail = acct?.email
-            val personId = acct?.id
-            val personPhoto: Uri? = acct?.photoUrl
-
-            Log.d(
-                "google",
-                personName + "," + personGivenName + "," + personFamilyName + "," + personEmail + "," + personId + "," + acct.idToken
-            )
-            tv_user_id.setText(personGivenName)
-            Glide.with(App.instance)
-                .load(personPhoto)
-                .into(iv_user_profile)
-        } else {
-            Log.d("google", "access fail")
-        }
-    }
 }
